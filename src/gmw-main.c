839dbf00eb7ede7d68ff87d69b921b6106e7a1de
Phr00t <phr00t@gmail.com>
2016-09-26 19:41:23 -0400

    Hopefully support 64GB sticks
    I have some 64GB sticks, and I could really use a build with this change...

--- src/gmw-main.c
+++ src/gmw-main.c
@@ -1646,7 +1646,7 @@ gmw_udisks_object_add (GmwPrivate *priv, GDBusObject *dbus_object)
 			 block_path, (guint) device_size);
 		return FALSE;
 	}
-	if (device_size > 1024 * 32) {
+	if (device_size > 1024 * 64) {
 		g_debug ("%s is too large [%u]",
 			 block_path, (guint) device_size);
 		return FALSE;
